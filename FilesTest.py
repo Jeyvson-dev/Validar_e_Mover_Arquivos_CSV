b1440e76ab95fd44b5596202652c088142db038b
Jeyvson Moreira <jeyvson0000@gmail.com>
2023-07-09 18:32:33 -0300

    Refatoração de variáveis

--- FilesTest.py
+++ FilesTest.py
@@ -11,41 +11,41 @@ data_hora_atual = str(datetime.now())
 data_hora_atual = data_hora_atual.replace(" ", "")
 current_time_string = re.sub(r'[^\w\s]', '', data_hora_atual)
 
-newName = ''
-currentDir = os.getcwd()+'/'
-fillesDir = 'FILLES/'
-sucessDir = 'SUCESS/'
-failDir = 'FAIL/'
-validateFille = []
+new_name = ''
+current_dir = os.getcwd()+'/'
+filles_dir = 'FILLES/'
+sucess_dir = 'SUCESS/'
+fail_dir = 'FAIL/'
+validate_fille = []
 
 #Varrer os arquivos dentro da pasta "FILLES"
-for ListFilles in util_class.list_filles(currentDir+fillesDir):
+for ListFilles in util_class.list_filles(current_dir+filles_dir):
     #Valida se o arquivo é CSV
     if(util_class.get_extension_filles(ListFilles) == '.csv'):
         try:
             #Varre todo o arquivo 'CSV' e valida se a quantidade de colunas é igual a 6 (seis)
-            for colunn in util_class.get_csv_lines(currentDir+fillesDir + ListFilles):
+            for colunn in util_class.get_csv_lines(current_dir+filles_dir + ListFilles):
                 if(len(colunn) != 6):
-                    validateFille.append('ERROR')
+                    validate_fille.append('ERROR')
                 else:
-                    validateFille.append('SUCESS')
+                    validate_fille.append('SUCESS')
             #Caso o arquivo não atenda a quantidade de colunas correta move para a pasta ("FAIL")
-            if 'ERROR' in validateFille:
-                newName = current_time_string + '_' + ListFilles
-                os.rename(currentDir + fillesDir + ListFilles, currentDir + fillesDir + newName)
-                shutil.move(currentDir + fillesDir + newName, failDir)
-                print(f'Fomato do arquivo inválido o arquivo {ListFilles} foi movido para a pasta de {failDir}')
+            if 'ERROR' in validate_fille:
+                new_name = current_time_string + '_' + ListFilles
+                os.rename(current_dir + filles_dir + ListFilles, current_dir + filles_dir + new_name)
+                shutil.move(current_dir + filles_dir + new_name, fail_dir)
+                print(f'Fomato do arquivo inválido o arquivo {ListFilles} foi movido para a pasta de {fail_dir}')
             else:
             # Caso passe por todas às validaçoes é movido para a pasta 'SUCESS'
-                newName = current_time_string + '_' + ListFilles
-                os.rename(currentDir + fillesDir + ListFilles, currentDir + fillesDir + newName)
-                shutil.move(currentDir + fillesDir + newName, failDir)
-                print(f'Segue O formato correto o arquivo {ListFilles} foi movido para a pasta de {sucessDir}')
+                new_name = current_time_string + '_' + ListFilles
+                os.rename(current_dir + filles_dir + ListFilles, current_dir + filles_dir + new_name)
+                shutil.move(current_dir + filles_dir + new_name, sucess_dir)
+                print(f'Segue O formato correto o arquivo {ListFilles} foi movido para a pasta de {sucess_dir}')
         except:
             print('Erro ao valdiar e Mover o arquivo')
     else:
         #Caso o arquivo não seja CSV move diretamente para a pasta "FAIL"
-        newName = current_time_string + '_' + ListFilles
-        os.rename(currentDir + fillesDir + ListFilles, currentDir + fillesDir + newName)
-        shutil.move(currentDir + fillesDir + newName, failDir)
-        print(f'Extensão do arquivo inválida o arquivo {newName} foi movido para a pasta de {failDir}')
+        new_name = current_time_string + '_' + ListFilles
+        os.rename(current_dir + filles_dir + ListFilles, current_dir + filles_dir + new_name)
+        shutil.move(current_dir + filles_dir + new_name, fail_dir)
+        print(f'Extensão do arquivo inválida o arquivo {new_name} foi movido para a pasta de {fail_dir}')
